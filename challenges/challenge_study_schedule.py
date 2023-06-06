6d534aff452335aafb4828d6b55e8214aacdbe58
Renan-Storrer <storrerrenan@gmail.com>
2023-06-06 17:23:11 -0300

    feat: implementa funções para o challenfe_study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,25 @@
+def period_permanece(start, end):
+    if start is None or end is None:
+        return False
+    if not isinstance(start, int) or not isinstance(end, int):
+        return False
+    elif start < 0 or end < 0:
+        return False
+    elif start > end:
+        return False
+
+    return True
+
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    count = 0
+    
+    if target_time is None:
+        return None
+
+    for start_time, end_time in permanence_period:
+        if not period_permanece(start_time, end_time):
+            return None
+        if (target_time >= start_time) and (target_time <= end_time):
+            count += 1
+
+    return count
