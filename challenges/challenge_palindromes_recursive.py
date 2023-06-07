a508282d486a81559f7430ff5cff51fb990860c4
Renan-Storrer <storrerrenan@gmail.com>
2023-06-07 15:20:11 -0300

    feat: implementa funções para o palindromes_recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,12 @@
-def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def letter_after(word, index_l, index_h):
+    if index_l >= index_h:
+        return True
+    if word[index_l] != word[index_h]:
+        return False
+    return letter_after(word, index_l + 1, index_h - 1)
+
+
+def is_palindrome_recursive(word, index_l, index_h):
+    if len(word) == 0:
+        return False
+    return letter_after(word, index_l, index_h)
